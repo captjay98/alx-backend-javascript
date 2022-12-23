e12a7a981bd03afb2323f5b3c06032af87bf6b74
CaptjayZorin <captjay98@gmail.com>
2022-12-23 21:45:34 +0100

    Task 4 fixed

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,6 +1,7 @@
 export default function returnHowManyArguments(...args) {
   let total = 0;
-  for (const arg in args) {
+  const data = Object.values(args);
+  for (let i = 0; i < data.length; i += 1) {
     total += 1;
   }
   return total;
