5e1551dcafd566b963f2a454f1653a47e43954c1
CaptjayZorin <captjay98@gmail.com>
2022-12-24 02:26:03 +0100

    whoops forogt Ten and Advanced

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+  const newArr = [];
+  for (const idx of array) {
+    newArr.push(appendString + idx);
+  }
+
+  return newArr;
+}
