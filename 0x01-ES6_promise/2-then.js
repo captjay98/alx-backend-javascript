ed62a846aeb5b22b97e47f2bfde110ad0f2e2ead
CaptjayZorin <captjay98@gmail.com>
2022-12-24 02:44:02 +0100

    Task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unreachable */
+export default function handleResponseFromAPI(promise) {
+  return promise.then(
+    () => ({
+      status: 200,
+      body: 'success',
+    }),
+  ).catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
