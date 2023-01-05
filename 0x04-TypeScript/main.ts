aa721cef0950550030879b934e8e31c5d35cffe2
CaptjayZorin <captjay98@gmail.com>
2023-01-05 02:16:41 +0100

    main task

---
+++ 0x04-TypeScript/main.ts
@@ -0,0 +1,24 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+
