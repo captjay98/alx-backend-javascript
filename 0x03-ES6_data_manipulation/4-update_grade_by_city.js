2ef3bac7c96574e1be3fb3aa3aca62ff0b052859
CaptjayZorin <captjay98@gmail.com>
2023-01-07 03:41:40 +0100

    fixed

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,11 @@
-export default function updateStudentGradeByCity(arg, city, newG) {
-  
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  return studentList.filter((element) => element.location === city).map((element) => {
+    element.grade = 'N/A'; // eslint-disable-line no-param-reassign
+    for (const grad of newGrades) {
+      if (grad.studentId === element.id) {
+        element.grade = grad.grade; // eslint-disable-line no-param-reassign
+      }
+    }
+    return element;
+  });
 }
