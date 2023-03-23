a58a8836bfe86e8673cc8e4fef075d1b9a10f5c4
captjay98wsl <captjay98@gmail.com>
2023-03-23 03:01:52 +0100

    redoing task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,64 +1,29 @@
 const express = require('express');
-const fs = require('fs');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
 
 const app = express();
-const path = process.argv[2];
 const port = 1245;
-
-const countStudents = (path) => {
-  const promise = (res, rej) => {
-    fs.readFile(path, 'utf8', (err, resData) => {
-      if (!err) {
-        const printOut = [];
-        let printItem; // item to printed
-        const data = resData.toString().split('\n');
-        let students = data.filter((item) => item);
-        students = students.map((item) => item.split(','));
-        printItem = `Number of students: ${students.length - 1}`;
-        console.log(printItem);
-        printOut.push(printItem);
-
-        const fields = {};
-        for (const student in students) {
-          if (student !== 0) {
-            if (!fields[students[student][3]]) {
-              fields[students[student][3]] = [];
-            }
-            fields[students[student][3]].push(students[student][0]);
-          }
-        }
-        delete fields.field;
-        for (const key of Object.keys(fields)) {
-          printItem = `Number of students in ${key}: ${
-            fields[key].length}. List: ${fields[key].join(', ')}`;
-          console.log(printItem);
-          printOut.push(printItem);
-        }
-        res(printOut);
-      } else {
-        rej(new Error('Cannot load the database'));
-      }
-    });
-  };
-
-  return new Promise(promise);
-};
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res, next) => {
-  countStudents(path)
-    .then((data) => {
-      res.send(`This is the list of our students\n${data.join('\n')}`);
-    })
-    .catch((error) => {
-      next(error);
-    });
+app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
+  }
 });
 
 app.listen(port, () => {
+  console.log();
 });
 
 module.exports = app;
